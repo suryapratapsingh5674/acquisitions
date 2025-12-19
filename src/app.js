9c164924d798c212e470f1249d802b8680168c77
surya <suryat6574@gmail.com>
2025-12-19 21:47:18 +0530

    add neon database with drizzle

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.status(200).send('hello from acquisition');
-})
+  res.status(200).send('hello from acquisition');
+});
 
 export default app;
